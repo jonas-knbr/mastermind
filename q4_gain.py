d8ba6b8bfb2299fd8770227ee6face2efb93851e
jonas-knbr <jonas@knauber.net>
2026-02-05 19:51:13 +0100

    Question 4 v2

--- q4_gain.py
+++ q4_gain.py
@@ -6,13 +6,12 @@ import codemaker1
 import codebreaker1
 import matplotlib.pyplot as plt
 
-nbr_parties = 10000 #int(input("Combien de parties ?\n> "))
+nbr_parties = 1000 #int(input("Combien de parties ?\n> "))
 parties = []
 
 for _ in range(nbr_parties):
     parties.append(play.play(codemaker1, codebreaker1, quiet=True))
 
-print(f"Minimum d'essais : {min(parties)}, maximum d'essais : {max(parties)}")
 plt.title(f"Histogramme du nombre d'essais réalisés par le codebreaker\n({nbr_parties} parties)")
 plt.xlabel("Nombre d'essais")
 plt.ylabel("Nombre d'occurrences")

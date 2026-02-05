5501ca152125776f47ec5b52ca879998bf7dd280
jonas-knbr <jonas@knauber.net>
2026-02-05 19:28:31 +0100

    Question 4

---
+++ codebreaker1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+import common
+
+
+def init():
+    global tentatives
+    tentatives = set()
+    return
+
+
+def codebreaker(evaluation_p):
+    # Combinaison au hasard
+    combinaison = ''.join(random.choices(common.COLORS, k=common.LENGTH))
+    
+    # Sortir une combinaison aléatoire jamais essayée auparavant
+    while combinaison in tentatives:
+        combinaison = ''.join(random.choices(common.COLORS, k=common.LENGTH))
+    # Ajouter la combinaison aux tentatives déjà esssayées
+    tentatives.add(combinaison)
+    
+    return combinaison
---
+++ q4_gain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import play
+import codemaker1
+import codebreaker1
+import matplotlib.pyplot as plt
+
+nbr_parties = 10000 #int(input("Combien de parties ?\n> "))
+parties = []
+
+for _ in range(nbr_parties):
+    parties.append(play.play(codemaker1, codebreaker1, quiet=True))
+
+print(f"Minimum d'essais : {min(parties)}, maximum d'essais : {max(parties)}")
+plt.title(f"Histogramme du nombre d'essais réalisés par le codebreaker\n({nbr_parties} parties)")
+plt.xlabel("Nombre d'essais")
+plt.ylabel("Nombre d'occurrences")
+plt.hist(parties)
+plt.savefig(f"q4_histogramme_{nbr_parties}_parties.png")
+plt.savefig(f"q4_histogramme.png")
+plt.show()
